9f468850814f6865986e20475e979eedbbd33afa
Ian <80067694+iansergio@users.noreply.github.com>
2026-09-21 11:31:19 -0300

    cria interface url repository

---
+++ backend/src/main/java/com/shortener/backend/repository/UrlRepository.java
@@ -0,0 +1,14 @@
+package com.shortener.backend.repository;
+
+import com.shortener.backend.model.Url;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UrlRepository extends JpaRepository<Url, Long> {
+
+    Optional<Url> findByShortCode(String shortCode);
+    boolean existsByShortCode(String shortCode);
+}
